8e051fb462247fc1653689ddbc2781ed19adc185
dmitrii.vokuev <dmitrii.vokuev@smaato.com>
2019-05-31 17:04:31 +0200

    init git

---
+++ gitLessons/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitLessons
+//
+//  Created by dmitrii on 31.05.19.
+//  Copyright © 2019 dmitrii. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
